ee3170f763778040034bd31aec32c2d4668bf6fd
Thomas Munzer <tmunzer@juniper.net>
2024-06-07 14:52:34 +0200

    main

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 ### PROVIDER
 provider "google" {
-  project = "advancedterraform" #replace this with your project-id
+  project = "advanced-terraform-425712" #replace this with your project-id
   region  = "us-central1"
   zone    = "us-central1-a"
 }
